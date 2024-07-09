7a526349385d3b604ead218a4b8fe5862dc9afa5
Mahesh Manandhar <manandhar533@gmail.com>
2024-07-09 19:09:18 +0545

    database connection and authentication

--- authenticate.php
+++ authenticate.php
@@ -1,27 +1,62 @@
 <?php
 session_start();
+require('database.php');
 
-$correct_username = "admin";
-$correct_email = "abc@gmail.com";
-$correct_password = "Admin@123";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = isset($_POST["name"]) ? $_POST["name"] : null;
-    $email = isset($_POST["email"]) ? $_POST["email"] : null;
-    $password = isset($_POST["password"]) ? $_POST["password"] : null;
+    $username1 = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-    if ($username === $correct_username && $email === $correct_email && $password === $correct_password) {
-        // Set session variables
-        $_SESSION["loggedin"] = true;
-        $_SESSION["username"] = $correct_username;
-
-        // Redirect to welcome page
-        header("Location: welcome.php");
-        exit();
+    $sql = "SELECT * FROM users ";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if ($password == $row["password"]) {
+            $_SESSION["username"] = $username1;
+            header("location: welcome.php");
+            exit();
+        } else {
+            echo "Invalid password";
+        }
     } else {
-        echo "Incorrect username or password.";
+        echo "Username not found";
     }
+
+    //     if (password_verify($password, $_POST["password"])) {
+    //         $_SESSION["username"] = $username1;
+    //         header("location:welcome.php");
+    //         exit();
+    //     } else {
+    //         echo "Invalid password";
+    //     }
+    // } else {
+    //     echo "Username not found";
+    // }
 }
+$conn->close();
+
+// $correct_username = "admin";
+// $correct_email = "abc@gmail.com";
+// $correct_password = "Admin@123";
+
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $username = isset($_POST["name"]) ? $_POST["name"] : null;
+//     $email = isset($_POST["email"]) ? $_POST["email"] : null;
+//     $password = isset($_POST["password"]) ? $_POST["password"] : null;
+
+//     if ($username === $correct_username && $email === $correct_email && $password === $correct_password) {
+//         // Set session variables
+//         $_SESSION["loggedin"] = true;
+//         $_SESSION["username"] = $correct_username;
+
+//         // Redirect to welcome page
+//         header("Location: welcome.php");
+//         exit();
+//     } else {
+//         echo "Incorrect username or password.";
+//     }
+// }
 ?>
 <!DOCTYPE html>
 <html lang="en">
---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "172.16.238.12";
+$username = "root";
+$password = "";
+$dbname = 'phpdb';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// echo "Connected successfully <br>";
